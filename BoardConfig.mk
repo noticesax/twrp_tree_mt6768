73d16dac7b80c26654349d80abe78d7d25a503aa
Arafattex <you@example.com>
2023-05-20 21:01:08 +0600

    Add system. prop

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -164,5 +164,8 @@ SELINUX_IGNORE_NEVERALLOWS := true
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
 
+# System.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
