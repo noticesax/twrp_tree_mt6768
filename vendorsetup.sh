b87cdf9113fe0820a6e7b2375837b66f04922154
Arafattex <141769738+Shas45558@users.noreply.github.com>
2024-03-03 17:26:23 +0600

    No need previously define

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -39,7 +39,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
  	export ALLOW_MISSING_DEPENDENCIES=true
 	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
 	export TARGET_DEVICE_ALT="shiva"
-	export FOX_TARGET_DEVICES="lancelot"
+	#export FOX_TARGET_DEVICES="lancelot"
 	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
 	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
 	export OF_QUICK_BACKUP_LIST="/boot;/data;"
